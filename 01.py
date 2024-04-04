5d73d68350936a4ec3eb82ab93f734ee4a34672b
12Ilnaz12 <159615880+12Ilnaz12@users.noreply.github.com>
2024-04-04 09:29:49 +0500

    Add files via upload
    Add works number 1

---
+++ 01.py
@@ -0,0 +1,11 @@
+text = input().split("~")
+colichestvo = 0
+print(colichestvo)
+for a1 in text:
+    a1.split()
+    for a2 in a1:
+        if a2 in "normal":
+            colichestvo += 1
+            break
+print(colichestvo)
+
